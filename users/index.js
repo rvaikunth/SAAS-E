52f5f6a8b88160d6eb436233d5589d30bdd8626f
Rhidam Vaikunth <rhidam@vl965-172-31-105-89.wireless.umass.edu>
2023-11-09 11:25:45 -0500

    adding client

--- users/index.js
+++ users/index.js
@@ -17,12 +17,13 @@ app.post('/events', (req, res) => {
 })
 
 app.post('/authenticateUser', (req, res) => {
-    const {type, data} = req.body;
+    const data = req.body;
 
     const userName = data.username;
     const password = data.password;
     console.log(userName)
     console.log(password)
+    
     if(credentials[userName] && credentials[userName] === password){
         res.status(200).send({
             token: userName
